1345a131085e0b180be4c5122ff6c6116ac32309
Parth Vasani <parth.vasani124578@gmail.com>
2018-04-03 14:37:40 +0530

    Title Chnages in all Pages

--- dashboard.php
+++ dashboard.php
@@ -20,7 +20,7 @@ $Row = mysql_fetch_array($Qry);
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Basic Bootstrap Template</title>
+    <title>Crud PDO System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Optional Bootstrap theme -->
