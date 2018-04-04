b8006804ed6ef17c3fdad799dd01f07c8462936a
sunita-patel <33368777+sunita-patel@users.noreply.github.com>
2018-04-04 09:48:28 +0530

    checking local to live
    checking local to live commit

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['login_email']) && !empty($_SESSION['login_email']))
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Testing System</title>
+    <title>Testing System to live</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Optional Bootstrap theme -->
